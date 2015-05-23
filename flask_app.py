18b56903d58d27bc3d185c4ee52706d747cf46d4
wwq0327 <wwq0327@gmail.com>
2015-05-23 16:57:09 +0800

    part2

---
+++ flask_app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask import Response
+
+flask_app = Flask('flaskApp')
+
+@flask_app.route('/hello')
+def hello_world():
+    return Response(
+        'Hello world from Flask!\n', mimetype='text/plain'
+    )
+
+app = flask_app.wsgi_app
---
+++ tornadoapp.py
@@ -0,0 +1,7 @@
+# import tornado.web
+
+# class HelloHandler(tornado.web.RequestHandler):
+#     def get(self):
+#         self.write('Hello, World by from Tornado!')
+
+# app = tornado.web.Application(handlers=[(r'/hello', HelloHandler)])
